8f7da763b27c958175c219601def5815898bfa85
ppogulailo <pavel.pogulailo@gmail.com>
2026-02-25 22:59:07 +0200

    [FAI-20] Reports (export + stats)

---
+++ src/reports/reports.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { RbacModule } from '../rbac/rbac.module';
+import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
+
+@Module({
+  imports: [PrismaModule, RbacModule],
+  controllers: [ReportsController],
+  providers: [ReportsService],
+})
+export class ReportsModule {}
